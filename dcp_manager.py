72d2c3b0c2e41ad45276a74392d22a4c4544baee
Mark McFate <summitt.dweller@gmail.com>
2017-02-06 21:16:59 -0600

    Progress.

--- dcp_manager.py
+++ dcp_manager.py
@@ -2,6 +2,7 @@
 # coding: utf8
 
 import os
+import fnmatch
 import argparse
 import logging
 import datetime
@@ -22,11 +23,217 @@ class dcp_manager():
   A logfile and a mail can be specified to be informed about operations.
   """
 
+  #--------------------------------
+  # fetchPKLAssets
+  #
+  # Fetch the list of assets from a PKL file.
+  #
+  # self.pkl - The PKL filename.
+  # self.assets - Returned array of assets found. Each asset is another array with an index equal to its 'id',
+  # a 'file' component and optional 'annotation'. Note that the PKL file itself MUST be prepended to the list of assets!
+  #
+  # Returns the AnnotationText value from the PKL or 0 if there was an error.
+  #
+  def fetchPKLAssets(self):
+    from xml.etree import ElementTree
+    with open(self.pkl, 'rt') as f:
+      tree = ElementTree.parse(f)
+      for node in tree.iter():
+        # print node.tag, node.attrib
+        if node.tag == 'Id':
+          id = node.attrib.get('text')
+        if node.tag == 'AnnotationText':
+          name = node.attrib.get('text')
+          
+      # Add the PKL file itself to self.assets
+      asset = {}
+      asset['id'] = id
+      asset['file'] = self.pkl
+      self.assets.append()
+
+  echo
+  "<hr/>";
+
+  // Get
+  the
+  PKL
+  Id.
+  if (isset($xml->Id)) {
+  $id = (string) $xml->Id;
+  
+} else {
+return FALSE;
+}
+
+// Get
+the
+PKL
+name
+from its AnnotationText.
+if (isset($xml->AnnotationText)) {
+$namePKL = (string) $xml->AnnotationText;
+} else {
+return FALSE;
+}
+
+// Seed
+the $assets
+list
+with the PKL file itself.
+$assets[$id] = array( );
+$assets[$id]['file'] = $path;
+$assets[$id]['annotation'] = $namePKL;
+
+$count = 0;
+
+// Logic for PKLs.
+if (isset($xml->AssetList)) {
+foreach($xml->AssetList->Asset as $a) {
+  list($seedDir, $file) = mam_split_path($path);
+$asset = array();
+
+// MAM...looks
+like
+the
+asset
+filename is held in the
+OriginalFileName.
+// Id and AnnotationText
+may
+also
+be
+significant
+so
+grab
+them!
+
+if (!isset($a->Id)) {
+echo "No Id for the file asset '$file'.  It is being skipped.<br>";
+mam_log_message(__FUNCTION__, "No Id for the file asset '$file'.  It is being skipped.");
+continue;
+} else {
+$id = (string) $a->Id;
+}
+
+if (isset($a->OriginalFileName)) {
+$f = (string) $a->OriginalFileName;
+$asset['file'] = $seedDir.$f;
+$count + +;
+} else {
+unset($asset[$id]);
+echo
+"No OriginalFileName for the asset '$file'.  It is being skipped.<br>";
+mam_log_message(__FUNCTION__, "No OriginalFileName for the asset '$file'.  It is being skipped.");
+continue;
+}
+
+if (isset($a->AnnotationText)) {
+$asset['annotation'] = (string) $a->AnnotationText;
+}
+
+echo
+"Found valid $f with an ID of $id.<br>";
+mam_log_message(__FUNCTION__, "Found valid $f in PKL $file.");
+$assets[$id] = $asset;
+}
+}
+
+// Count
+the $assets
+for $id.If less than 3 exist then the package is incomplete and should be deleted.
+if ($count < 3) {
+$script = "/tmp/dcp_manager.sh";
+echo "<br><b>Asset count for $namePKL is only $count.  This PKL is incomplete and should be removed!</b><br>";
+$cmd = "unlink $path";
+file_put_contents($script, $cmd."\n", FILE_APPEND | LOCK_EX);
+}
+
+return $namePKL;
+}
+
+    
+
+  # -------------------------------
+  # Catalog - Prepare an ASSETMAP file for the self.source directory.
+  def catalog(self):
+    # Check for an ASSETMAP file in self.dest
+    if os.path.exists(self.destAssetMap):
+      self.logger.warning("Be advised, the target ASSETMAP file already exists.  It will be replaced.")
+      os.remove(self.destAssetMap)
+    else:
+      self.logger.info("No target " + self.destAssetMap + " found so a new one will be created.")
+  
+    self.initAssetMap()     # Initialize a new ASSETMAP
+    
+    # Find all PKL files in the source directory
+    pkls = []
+    for root, dirnames, filenames in os.walk(self.source):
+      for filename in fnmatch.filter(filenames, ['*.PKL.xml', '*.pkl.xml']):
+        pkls.append.join(root, filename)
+
+    countPKL = 0
+    countAssets = 0
+
+    # Loop on the found PKL files
+    for pkl in pkls:
+      self.pkl = pkl
+      self.assets = []
+      name = self.fetchPKLAssets()
+      
+      each($files as $file) {
+    $assets = array();
+    $countPKL + +;
+
+    // Fetch
+    the
+    assets
+    for each PKL found.
+    $name = fetch_PKL_assets($file, $assets);
+    echo "<div style='text-indent:0.5em;' class='prompt'>$name</div>";
+    echo "<div style='text-indent:0; margin-top:1em;'>Found PKL file '$file' for examination.</div>";
+
+    $Assets = array( );
+
+    // Pre-scan the list of PKL assets.If any files are missing, issue a warning.
+
+    $complete = TRUE;
+    foreach ($assets as $id = > $asset) {
+    $Assets[$id] = $asset;
+    $file = $asset['file'];
+    $basename = basename($file);
+    if (!file_exists($file)) {
+    echo "<div style='text-indent:3em;'
+
+    class ='warning-message' > Could not find asset '$file'! This package may be incomplete! < / div > ";
+
+    if (!$path = globForAsset($dest, $basename)) {
+    echo "<div style='text-indent:3em;'
+
+    class ='error-message' > Could not find asset file '$basename'! This package IS incomplete! < / div > ";
+  
+    } else {
+    echo
+    "<div style='text-indent:1em;'>Found target file $basename in $path!</div>";
+    $Assets[$id]['file'] = $path.$basename;
+  
+  }
+  } else {
+    echo
+  "<div style='text-indent:1em;'>Found target file $basename in $dest.</div>";
+  $Assets[$id]['file'] = $dest.$basename;
+  }
+  }
+
+
+  self.return_value = 0
+    return "This is the 'catalog' function.  A new initial ASSETMAP file has been created."
+
+
   #------------------------------
   def initAssetMap(self):
     root = ET.Element('AssetMap')
     annotationText = ET.SubElement(root, 'AnnotationText')
-    annotationText.text = 'Initial_AssetMap'
+    annotationText.text = 'DCP Manager Build ' + self.date
     volumeCount = ET.SubElement(root, 'VolumeCount')
     volumeCount.text = '1'
     issueDate = ET.SubElement(root, 'IssueDate')
@@ -39,6 +246,7 @@ class dcp_manager():
     # print ET.tostring(root, pretty_print=True, xml_declaration=True)
     xml = ET.ElementTree(root)
     xml.write(self.destAssetMap, pretty_print=True, xml_declaration=True)
+
 
   #-------------------------------
   #if __name__ == '__main__':
@@ -173,21 +381,6 @@ class dcp_manager():
         self.logger.error("Sending mail failed! Error: %s." % e)
       finally:
         self.conn.close()
-
-
-  # -------------------------------
-  # Catalog - Prepare an ASSETMAP file for the self.source directory.
-  def catalog(self):
-    # Check for an ASSETMAP file in self.dest
-    if os.path.exists(self.destAssetMap):
-      self.logger.warning("Be advised, the target ASSETMAP file already exists.  It will be replaced.")
-      os.remove(self.destAssetMap)
-    else:
-      self.logger.info("No target " + self.destAssetMap + " found so a new one will be created.")
-      
-    self.initAssetMap()
-    self.return_value = 0
-    return "This is the 'catalog' function.  A new initial ASSETMAP file has been created."
 
 
   #-------------------------------
